624edcefef7dc9e47c69c778508da98b578d9fad
lh12g22 <lh12g22@soton.ac.uk>
2023-07-05 19:12:09 +0100

    setup boilerplate, cargo and accept loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use async_std::{
+    prelude::*,
+    task,
+    net::{TcpListener, ToSocketAddrs},
+};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+
 fn main() {
     println!("Hello, world!");
 }
